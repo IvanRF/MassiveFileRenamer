015b42b81499f596369983f9d1c2516b6a00e4ce
IvanRF <ivanridao@gmail.com>
2014-03-24 17:56:45 -0300

    Added notes about capturing groups

--- src/com/ivanrf/renamer/visual/SyntaxPanel.java
+++ src/com/ivanrf/renamer/visual/SyntaxPanel.java
@@ -17,9 +17,11 @@
 package com.ivanrf.renamer.visual;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Insets;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class SyntaxPanel extends JPanel {
@@ -29,6 +31,7 @@ public class SyntaxPanel extends JPanel {
 	public SyntaxPanel() {
 		super();
 		initGUI();
+		setPreferredSize(new Dimension(600, 500));
 	}
 	
 	private void initGUI() {
@@ -38,34 +41,46 @@ public class SyntaxPanel extends JPanel {
 		textArea.setEditable(false);
 		textArea.setMargin(new Insets(5,5,5,5));
 		textArea.setText(
-				"Add \\ before this characters (  [  {  \\  ^  $  |  )  ?  *  +  . if you want to replace them."+"\n\n"+
-				"Character Classes"+"\n"+		   
-				"[abc]	a, b, or c (simple class)"+"\n"+ 	   
-				"[^abc] 	Any character except a, b, or c (negation)"+"\n"+ 	   
-				"[a-zA-Z] 	a through z, or A through Z, inclusive (range)"+"\n"+ 	   
-				"[a-d[m-p]]	a through d, or m through p: [a-dm-p] (union)"+"\n"+ 	   
-				"[a-z&&[def]] 	d, e, or f (intersection)"+"\n"+ 	   
-				"[a-z&&[^bc]] 	a through z, except for b and c: [ad-z] (subtraction)"+"\n"+ 	   
-				"[a-z&&[^m-p]]	a through z, and not m through p: [a-lq-z] (subtraction)"+"\n"+ 	 
-				"\n"+  
-				"Predefined Character Classes"+"\n"+		   
-				". 	Any character (may or may not match line terminators)"+"\n"+ 	   
-				"\\d 	A digit: [0-9]"+"\n"+ 	   
-				"\\D 	A non-digit: [^0-9]"+"\n"+ 	   
-				"\\s 	A whitespace character: ["+"\\ t \\ n \\x0B \\f\\r]"+"\n"+ 	   
-				"\\S 	A non-whitespace character: [^\\s]"+"\n"+ 	   
-				"\\w 	A word character: [a-zA-Z_0-9]"+"\n"+	   
-				"\\W 	A non-word character: [^\\w]"+"\n"+ 	 
-				"\n"+
-				"Quantifiers			Meaning"+"\n"+	   
-				"Greedy	Reluctant	Possessive"+"\n"+	 	   
-				"X?	X??	X?+	X, once or not at all"+"\n"+	   
-				"X*	X*?	X*+	X, zero or more times"+"\n"+	   
-				"X+	X+?	X++	X, one or more times"+"\n"+	   
-				"X{n} 	X{n}? 	X{n}+ 	X, exactly n times"+"\n"+	   
-				"X{n,}	X{n,}?	X{n,}+	X, at least n times"+"\n"+   
-				"X{n,m}	X{n,m}?	X{n,m}+	X, at least n but not more than m times"
+				"Quick notes:" + "\n" +
+				" • Add \\ before this characters (  [  {  \\  ^  $  |  )  ?  *  +  . if you want to replace them." + "\n" +
+				" • Use $n for capturing groups (instead of \\n)." + "\n" +
+				"\n" +
+				"Character Classes" + "\n" +
+				"[abc]	a, b, or c (simple class)" + "\n" +
+				"[^abc] 	Any character except a, b, or c (negation)" + "\n" +
+				"[a-zA-Z] 	a through z, or A through Z, inclusive (range)" + "\n" +
+				"[a-d[m-p]]	a through d, or m through p: [a-dm-p] (union)" + "\n" +
+				"[a-z&&[def]] 	d, e, or f (intersection)" + "\n" +
+				"[a-z&&[^bc]] 	a through z, except for b and c: [ad-z] (subtraction)" + "\n" +
+				"[a-z&&[^m-p]]	a through z, and not m through p: [a-lq-z] (subtraction)" + "\n" +
+				"\n" +
+				"Predefined Character Classes" + "\n" +
+				". 	Any character (may or may not match line terminators)" + "\n" +
+				"\\d 	A digit: [0-9]" + "\n" +
+				"\\D 	A non-digit: [^0-9]" + "\n" +
+				"\\s 	A whitespace character: ["+"\\ t \\ n \\x0B \\f\\r]" + "\n" +
+				"\\S 	A non-whitespace character: [^\\s]" + "\n" +
+				"\\w 	A word character: [a-zA-Z_0-9]" + "\n" +
+				"\\W 	A non-word character: [^\\w]" + "\n" +
+				"\n" +
+				"Quantifiers			Meaning" + "\n" +
+				"Greedy	Reluctant	Possessive" + "\n" +
+				"X?	X??	X?+	X, once or not at all" + "\n" +
+				"X*	X*?	X*+	X, zero or more times" + "\n" +
+				"X+	X+?	X++	X, one or more times" + "\n" +
+				"X{n} 	X{n}? 	X{n}+ 	X, exactly n times" + "\n" +
+				"X{n,}	X{n,}?	X{n,}+	X, at least n times" + "\n" +
+				"X{n,m}	X{n,m}?	X{n,m}+	X, at least n but not more than m times" + "\n" +
+				"\n" +
+				"Logical operators" + "\n" +
+				"XY	X followed by Y" + "\n" +
+				"X|Y	Either X or Y" + "\n" +
+				"(X)	X, as a capturing group" + "\n" +
+				"\n" +
+				"Back references" + "\n" +
+				"$n	Whatever the nth capturing group matched"
 		);
-		this.add(textArea, BorderLayout.CENTER);
+		this.add(new JScrollPane(textArea), BorderLayout.CENTER);
+		textArea.setCaretPosition(0);
 	}
 }
